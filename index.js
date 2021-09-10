0a85b3fb32fc4a1c18a4ba2c7dba07bab3064828
Samuel <samlee1097@yahoo.com>
2021-09-09 23:41:26 -0400

    commit tree dbe8a940ccac8e37e8d9d9bfc612b691d8c7bd7e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(nameData, emailData){
+    return fetch("http://localhost:3000/users", {
+        method : 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: nameData,
+            email: emailData
+        })
+    })
+    .then(resp => resp.json())
+    .then(function(data){
+        const idHeader = document.createElement("h1");
+        idHeader.innerText = `Hello ${data.name}, this is your ID: ${data.id}`;
+        document.getElementById("body").append(idHeader);
+    })
+    .catch(function(error){
+        const errorHeader = document.createElement("h1");
+        errorHeader.innerText = `Check your console to see the error message :(`;
+        document.getElementById("body").append(errorHeader);
+        console.log(error.message);
+    })
+};
